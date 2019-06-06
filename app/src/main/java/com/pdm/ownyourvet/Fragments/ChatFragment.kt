368ddf0b0608366c1e61038de7886ffd1d901467
EdwinLovo <00120617@uca.edu.sv>
2019-06-06 07:29:48 -0600

    Firestore integration, add message ready

--- app/src/main/java/com/pdm/ownyourvet/Fragments/ChatFragment.kt
+++ app/src/main/java/com/pdm/ownyourvet/Fragments/ChatFragment.kt
@@ -1,19 +1,100 @@
 package com.pdm.ownyourvet.Fragments
 
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.pdm.ownyourvet.Adapters.ChatAdapter
+import com.pdm.ownyourvet.Models.Message
 
 import com.pdm.ownyourvet.R
+import com.pdm.ownyourvet.toast
+import kotlinx.android.synthetic.main.fragment_chat.*
+import kotlinx.android.synthetic.main.fragment_chat.view.*
+import java.util.*
+import kotlin.collections.HashMap
 
 class ChatFragment : Fragment() {
 
+    private lateinit var _view: View
+
+    private val store:FirebaseFirestore = FirebaseFirestore.getInstance()
+    private lateinit var chatDBRef:CollectionReference
+
+    private val mAuth:FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var currentUser:FirebaseUser
+
+    private lateinit var adapter:ChatAdapter
+    private var messageList:ArrayList<Message> = ArrayList()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        _view = inflater.inflate(R.layout.fragment_chat, container, false)
+
+        setUpChatDB()
+        setUpCurrentUser()
+        setUpRecyclerView()
+        setUpChatBtn()
+
+        return _view
+    }
+
+    private fun setUpChatDB(){
+        chatDBRef = store.collection("chat")
+    }
+
+    private fun setUpCurrentUser(){
+        currentUser = mAuth.currentUser!!
+    }
+
+    private fun setUpRecyclerView(){
+        val layoutManager = LinearLayoutManager(context)
+        adapter = ChatAdapter(messageList,currentUser.uid)
+
+        _view.recyclerviewChat.setHasFixedSize(true)
+        _view.recyclerviewChat.layoutManager = layoutManager
+        _view.recyclerviewChat.itemAnimator = DefaultItemAnimator()
+        _view.recyclerviewChat.adapter = adapter
+    }
+
+    private fun setUpChatBtn(){
+        _view.buttonSend.setOnClickListener {
+            val messageText = _view.editTextMessage.text.toString()
+            if (messageText.isNotEmpty()){
+                val message = Message(currentUser.uid, messageText, currentUser.photoUrl.toString(), Date())
+                saveMessage(message)
+                _view.editTextMessage.setText("")
+            }
+        }
+    }
+
+    private fun saveMessage(message: Message){
+        val newMessage = HashMap<String,Any>()
+        newMessage["authorId"] = message.authorId
+        newMessage["message"] = message.message
+        newMessage["profileImageURL"] = message.profileImageURL
+        newMessage["sentAt"] = message.sentAt
+
+        chatDBRef.add(newMessage)
+            .addOnCompleteListener {
+            activity!!.toast("Message added!")
+            }
+            .addOnFailureListener {
+                activity!!.toast("Message error, try again!")
+            }
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(uri: Uri)
     }
 
 
--- app/src/main/java/com/pdm/ownyourvet/MainActivity.kt
+++ app/src/main/java/com/pdm/ownyourvet/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.pdm.ownyourvet
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import com.pdm.ownyourvet.Activities.LoginActivity
+import com.pdm.ownyourvet.Fragments.ChatFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ChatFragment.OnFragmentInteractionListener {
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     private val mAuth: FirebaseAuth by lazy {FirebaseAuth.getInstance()}
 
@@ -14,9 +20,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_SignOut_MainActivity.setOnClickListener {
+        val chatFragment = ChatFragment()
+        supportFragmentManager.beginTransaction().add(R.id.framelayoutContainer,chatFragment).commit()
+
+        /*button_SignOut_MainActivity.setOnClickListener {
             mAuth.signOut()
             goToActivity<LoginActivity>()
-        }
+        }*/
     }
 }
