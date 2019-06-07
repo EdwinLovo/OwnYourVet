5d28fcdb9c3d98d363e675c67f11933d07d647f9
EdwinLovo <00120617@uca.edu.sv>
2019-06-07 13:53:49 -0600

    Navigation editor finished, logout working

--- app/src/main/java/com/pdm/ownyourvet/MainActivity.kt
+++ app/src/main/java/com/pdm/ownyourvet/MainActivity.kt
@@ -3,6 +3,11 @@ package com.pdm.ownyourvet
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.google.firebase.auth.FirebaseAuth
 import com.pdm.ownyourvet.Activities.LoginActivity
 import com.pdm.ownyourvet.Fragments.ChatFragment
@@ -19,13 +24,59 @@ class MainActivity : AppCompatActivity(), ChatFragment.OnFragmentInteractionList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        setUpBottomNavMenu(navController)
+        setUpSideNavigationMenu(navController)
+        setUpActionBar(navController)
+/*
         val chatFragment = ChatFragment()
-        supportFragmentManager.beginTransaction().add(R.id.framelayoutContainer,chatFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.framelayoutContainer,chatFragment).commit()*/
 
         /*button_SignOut_MainActivity.setOnClickListener {
             mAuth.signOut()
             goToActivity<LoginActivity>()
         }*/
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.exit -> {
+                mAuth.signOut()
+                goToActivity<LoginActivity>()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawer_layout)
+    }
+
+    private fun setUpBottomNavMenu(navController: NavController){
+        bottom_nav?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setUpSideNavigationMenu(navController: NavController){
+        nav_view?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setUpActionBar(navController: NavController){
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+    }
+
 }
